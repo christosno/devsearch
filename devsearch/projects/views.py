05e226d9856064218a929963b98012e36fe9b2e9
Christos Nounis <christosno1984@gmail.com>
2021-09-02 23:31:45 +0300

    add model forms

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -1,24 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Project
+from .forms import ProjectForm
 
-projectsList = [
-    {
-        'id':"1",
-        'title':'Ecommerce Website',
-        'description':'Fully functional ecommerce'
-    },
-    {
-        'id':"2",
-        'title':'Portofolio wedsite',
-        'description':'This is a project where i built out my portofolio'
-    },
-    {
-        'id':"3",
-        'title':'Social Network',
-        'description':'Awesome open project i am still working'
-    }
-]
 
 def projects(request):
 
@@ -33,5 +17,12 @@ def project(request, pk):
     
     projectObj = Project.objects.get(id=pk)
     
-
     return render(request, 'projects/single-project.html', {'project':projectObj,})
+
+
+def createProject(request):
+    form = ProjectForm()
+    context = {
+        'form':form
+    }
+    return render(request, 'projects/projects_form.html', context)
