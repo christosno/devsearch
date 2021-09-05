5cddc9161c48fd54c4def777957d776d8856338b
Christos Nounis <christosno1984@gmail.com>
2021-09-05 21:47:17 +0300

    add signals

---
+++ devsearch/users/signals.py
@@ -0,0 +1,24 @@
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+from django.contrib.auth.models import User
+from.models import Profile
+
+# create a linsener when create a user, we create and a profile
+def createProfile(sender, instance, created, **kwargs):
+    if created:
+        user = instance
+        profile = Profile.objects.create(
+            user=user,
+            username=user.username,
+            email=user.email,
+            name=user.first_name,
+        )
+
+# delete the user if deleted the profile
+def deleteUser(sender, instance, **kwargs):
+    user = instance.user
+    user.delete()
+    print('Deleting user ...')
+
+post_save.connect(createProfile, sender=User)
+post_delete.connect(deleteUser, sender=Profile)
