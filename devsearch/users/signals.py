28ad38468902f9fc7458390eadc6adc82caa6c43
Christos Nounis <christosno1984@gmail.com>
2021-09-10 14:17:31 +0300

    add welcome emails

--- devsearch/users/signals.py
+++ devsearch/users/signals.py
@@ -1,7 +1,10 @@
+from django.core.checks import messages
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from django.contrib.auth.models import User
 from.models import Profile
+from django.core.mail import send_mail
+from django.conf import settings
 
 # create a linsener when create a user, we create and a profile
 def createProfile(sender, instance, created, **kwargs):
@@ -13,6 +16,21 @@ def createProfile(sender, instance, created, **kwargs):
             email=user.email,
             name=user.first_name,
         )
+
+        subject = 'Welcame to DevSerach'
+        message = 'We are glad you are here'
+
+        send_mail(
+            subject,
+            message,
+            settings.EMAIL_HOST_USER,
+            [profile.email],
+            fail_silently=False,
+        )
+
+
+
+
 
 def updateUser(sender, instance, created, **kwargs):
     profile = instance
