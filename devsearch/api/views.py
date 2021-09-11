2c915f64e1cfafa54f746f8c758e36cbcfa833b0
Christos Nounis <christosno1984@gmail.com>
2021-09-11 13:37:19 +0300

    authentication with jwt

--- devsearch/api/views.py
+++ devsearch/api/views.py
@@ -1,5 +1,6 @@
 
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 from .serializers import ProojectSeriliazer
 from projects.models import Project
